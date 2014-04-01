a5ce1b1a1726c5f6b16d9620e2fbb3cb88db22e9
mtgto <hogerappa@gmail.com>
2014-04-01 13:59:04 +0900

    環境変数から設定を読むようにした。

--- app/controllers/BaseController.scala
+++ app/controllers/BaseController.scala
@@ -9,13 +9,14 @@ import play.api.mvc.Controller
 trait BaseController {
   self: Controller =>
 
-  protected[this] def getConfiguration(name: String): String = Play.current.configuration.getString(name).get
+  protected[this] def getConfiguration(configName: String, envName: String): String =
+    Option(System.getenv(envName)).getOrElse(Play.current.configuration.getString(configName).get)
 
   protected[this] implicit val context = SyncEntityIOContext
 
   protected[this] val garoonClient: GaroonClient = new GaroonClient(
-    getConfiguration("garoon.username"),
-    getConfiguration("garoon.password"),
-    new URI(getConfiguration("garoon.uri"))
+    getConfiguration("garoon.username", "GAROON_USERNAME"),
+    getConfiguration("garoon.password", "GAROON_PASSWORD"),
+    new URI(getConfiguration("garoon.uri", "GAROON_URI"))
   )
 }
